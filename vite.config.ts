b6f8b2d6aa7a38b03c21c4879add10f8243e9d94
pankajbc99-dev <pankaj.bc99@gmail.com>
2026-01-12 18:12:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+    'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY),
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
   build: {
     outDir: 'dist',
+    target: 'esnext',
+    minify: 'esbuild'
   }
 });
